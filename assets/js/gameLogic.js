961b27016aa15f02212a3b07fb1593497e3ffe2d
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-30 23:19:24 -0700

    Added id to the start-content div. Added function for the clock time, and quizStart()

--- assets/js/gameLogic.js
+++ assets/js/gameLogic.js
@@ -1,3 +1,30 @@
-var timerId;
-
 var timeEl = document.getElementById("time");
+var questionEl = document.getElementById("questions");
+var choicesEl = document.getElementById("choices");
+var startBtn = document.getElementById("start-button");
+
+var timerId;
+var questionIndex = 0;
+var time = questions.length * 10;
+
+function clockTime() {
+    time--;
+    timeEl.textContent = time;
+
+    // placeholder for future function
+    // if (time <= 0) {
+    //     endQuiz();
+    // }
+}
+
+function quizStart() {
+    var startScreen = document.getElementById("start-screen");
+    startScreen.setAttribute("class", "start hide__content");
+
+    questionEl.setAttribute("class", " ");
+    timerId = setInterval(function () {
+        clockTime();
+    }, 1000);
+    timeEl.textContent = time;
+
+}
