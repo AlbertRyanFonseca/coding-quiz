7a50e2f5582484068ce61978b438a7bb7d5768e9
AlbertFonseca-ucla <beezyscoding@gmail.com>
2021-09-30 22:42:30 -0700

    Add questions to the questions.js

---
+++ assets/js/questions.js
@@ -0,0 +1,37 @@
+var questions = [
+    {
+        title: "How do we call JavaScript through our HTML document?",
+        choices: ["<script>", "<link>", "<javascript>", "<document.script>"],
+        answer: "<script>"
+    },
+    {
+        title: "What is a useful command that is used as a tool that programmers use to debug and/or print content for JavaScript?",
+        choices: ["consolelog", "console.log", "log.console", "terminal"],
+        answer: "console.log"
+    },
+    {
+        title: "How do call the external JS file from the <script> tag?",
+        choices: ["<script src=", "<script name=", "<script href=", "<script file="],
+        answer: "<script src="
+    },
+    {
+        title: "How do you call this function: function myFunction() ?",
+        choices: ["myFunction;", "myFunction();", "(myFunction)", "myfunction()"],
+        answer: "myFunction()"
+    },
+    {
+        title: "Besides the FOR loop, what is another fundamental loop in JS?",
+        choices: ["If", "else", "while", "i++"],
+        answer: "while"
+    },
+    {
+        title: "How do you start a comment on one line with JS",
+        choices: ["<!-- -->", "/*", " ' ", "//"],
+        answer: "//"
+    },
+    {
+        title: "How do you start an array in JS?",
+        choices: ["()", "[]", "{}", "[}"],
+        answer: "[]"
+    }
+]
